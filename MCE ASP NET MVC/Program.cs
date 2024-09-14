8e36ae0f2ba67b240919f62bf3ee041dc8c7357f
realskrip <19eazy@gmail.com>
2024-09-15 00:26:04 +0700

    Initial commit

---
+++ MCE ASP NET MVC/Program.cs
@@ -0,0 +1,15 @@
+namespace MCE_ASP_NET_MVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
