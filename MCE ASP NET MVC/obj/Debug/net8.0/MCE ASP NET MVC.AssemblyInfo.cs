274724034a1200da376246ebaa63ea74f1861b4e
realskrip <19eazy@gmail.com>
2024-09-15 13:22:04 +0700

    0.1.1 feature Added _Layout.cshtml, _ViewImports.cshtml, _ViewStart.cshtml.

--- MCE ASP NET MVC/obj/Debug/net8.0/MCE ASP NET MVC.AssemblyInfo.cs
+++ MCE ASP NET MVC/obj/Debug/net8.0/MCE ASP NET MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MCE ASP NET MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+815a11daf005404664b0eb6d88cf257404b8d8b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68c4f36e70ad98e5ee468f5e4146a2042f395372")]
 [assembly: System.Reflection.AssemblyProductAttribute("MCE ASP NET MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MCE ASP NET MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
