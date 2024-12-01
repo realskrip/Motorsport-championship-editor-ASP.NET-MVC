8abb8f9d044a7ac04cb59d28a17a273f7d62b234
realskrip <19eazy@gmail.com>
2024-12-01 22:38:04 +0700

    Added Empty.txt files.

--- MCE ASP NET MVC/obj/Debug/net8.0/MCE ASP NET MVC.AssemblyInfo.cs
+++ MCE ASP NET MVC/obj/Debug/net8.0/MCE ASP NET MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MCE ASP NET MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68c4f36e70ad98e5ee468f5e4146a2042f395372")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e89d0d0647ead9aaab602756fad3019ef988fdd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MCE ASP NET MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MCE ASP NET MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
