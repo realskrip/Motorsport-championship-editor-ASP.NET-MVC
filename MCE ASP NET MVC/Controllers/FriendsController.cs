9ca3e84342dc83b2a13b309dff97d767e112a4d4
realskrip <19eazy@gmail.com>
2025-04-17 22:24:49 +0700

    Added Friends button

---
+++ MCE ASP NET MVC/Controllers/FriendsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MCE_ASP_NET_MVC.Controllers
+{
+    public class FriendsController : Controller
+    {
+        public IActionResult ShowFriendList()
+        {
+            return View("Friends");
+        }
+    }
+}
