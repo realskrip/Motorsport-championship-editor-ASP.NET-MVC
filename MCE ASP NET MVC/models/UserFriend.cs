17bc8d507cf5fd53328d55bf96eaddbcc93207d3
Скрипников Данила Андреевич <dskripnikov@intant.ru>
2025-04-21 16:06:33 +0700

    Added UserFriend model

---
+++ MCE ASP NET MVC/models/UserFriend.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MCE_ASP_NET_MVC.Models
+{
+    public class UserFriend
+    {
+        [Key, Column(Order = 0)]
+        public int userId { get; set; }
+        
+        [Key, Column(Order = 1)]
+        public int friendId { get; set; }
+    }
+}
