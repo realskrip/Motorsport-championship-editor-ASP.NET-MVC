ff89f931412aa005f977508bd1e0d669afc11bd6
Скрипников Данила Андреевич <dskripnikov@intant.ru>
2025-04-22 12:58:23 +0700

    Added NotificationsController

---
+++ MCE ASP NET MVC/Controllers/NotificationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MCE_ASP_NET_MVC.Controllers
+{
+    public class NotificationsController : Controller
+    {
+        public IActionResult ShowNotificationsList()
+        {
+            return View();
+        }
+    }
+}
