604ba6d8a72cb519426eac58704f63ba381288c2
Скрипников Данила Андреевич <dskripnikov@intant.ru>
2025-04-23 12:12:22 +0700

    Added xUnit, Moq and HomeControllerTests

---
+++ MCE ASP NET MVC.Tests/HomeControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using MCE_ASP_NET_MVC.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MCE_ASP_NET_MVC.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexViewResultNotNull()
+        {
+            // Arrange
+            HomeController homeController = new HomeController();
+
+            // Act
+            ViewResult result = homeController.Index() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
